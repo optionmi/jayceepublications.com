3d1ca5f31a62ee0daf927f2312968408dfca3e5d
Vikalp Mishra <vikalpmishra783@gmail.com>
2024-10-08 19:29:29 +0530

    make features dynamic

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -12,6 +12,28 @@ class HomeController extends Controller
 {
     public function index()
     {
+        $features = [
+            [
+                'name' => 'Interactive Digital Animations',
+                'desc' => 'Engage students with dynamic, visually appealing animations that bring complex concepts to life.'
+            ],
+            [
+                'name' => 'Instant Answer Keys',
+                'desc' => 'Provide educators and students with seamless access to answer keys, ensuring faster learning and more effective assessment.'
+            ],
+            [
+                'name' => 'Test Paper Generator',
+                'desc' => 'Simplify the assessment process with our intuitive test paper generator, offering customized tests that cater to various educational levels.'
+            ],
+            [
+                'name' => 'Ebook/Flipbook',
+                'desc' => 'Ebook/Flipbook is a powerful tool that allows educators to create and share interactive books in an efficient and intuitive manner.'
+            ],
+            [
+                'name' => 'Youtube',
+                'desc' => 'With our easy-to-use YouTube support, educators can easily share videos with students and parents.'
+            ],
+        ];
         $articles = Article::orderByDesc('created_at')->get();
         $series = collect(File::allFiles(public_path('series')))
             ->map(function ($fileInfo) {
@@ -21,7 +43,7 @@ class HomeController extends Controller
 
         $authors =  Author::limit(4)->get();
 
-        return view('web.home', compact('articles', 'series', 'authors'));
+        return view('web.home', compact('features', 'articles', 'series', 'authors'));
     }
 
     public function showArticle(Article $article)
